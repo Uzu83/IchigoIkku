9d0c114d44d1662f1f2800a4a73869defcd5482a
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-09 19:50:30 +0900

    FirstCommit MakingHaikuCard

---
+++ IchigoIkku/Presentation/Scenes/HaikuCreation/HaikuCreationView.swift
@@ -0,0 +1,19 @@
+//
+//  HaikuCreationView.swift
+//  IchigoIkku
+//
+//  Created by Uzu83 on 2024/11/09
+//  
+//
+
+import SwiftUI
+
+struct HaikuCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HaikuCreationView()
+}
