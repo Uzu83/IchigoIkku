742606bd204f483ca8827b8f1df281e3b98608ae
Toshiki Togo <108942850+Uzu83@users.noreply.github.com>
2024-11-09 16:35:17 +0900

    Initial Commit

---
+++ IchigoIkku/IchigoIkkuApp.swift
@@ -0,0 +1,18 @@
+//
+//  IchigoIkkuApp.swift
+//  IchigoIkku
+//
+//  Created by Uzu83 on 2024/11/09
+//  
+//
+
+import SwiftUI
+
+@main
+struct IchigoIkkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
